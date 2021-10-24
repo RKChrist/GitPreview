88c9cbcc6754e87ad6821065ee1bd7b797dfceac
RKChrist <roo12ney@gmail.com>
2021-10-24 16:43:06 +0200

    Added a new Project

---
+++ GitPreview/GitPreview/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GitPreview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
